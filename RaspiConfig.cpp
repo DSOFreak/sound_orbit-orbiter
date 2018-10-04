e385ceed9828410c233d6e2e7e3dcb75bef67817
Tim Fischer <tim.fischer@artorg.unibe.ch>
2018-10-04 18:05:00 +0200

    adapted wheel sizes dsd 360 now much more accurate

--- RaspiConfig.cpp
+++ RaspiConfig.cpp
@@ -4,7 +4,7 @@
 //id 0: ending 25.
 //id 1: ending 26.
 // id 2: ending 22
-const int RaspiConfig::ownIndex =1; //0ok2ok
+const int RaspiConfig::ownIndex =0; //0ok2ok
 
 
 
@@ -15,15 +15,15 @@ RaspiConfig::RaspiConfig()
 {
 	if (ownIndex == 2)
 	{
-		fWheelPerimeterRaspi = 0.0303359 * 3.1415;
+		fWheelPerimeterRaspi = 0.0301175 * 3.1415;
 	}
 	else if (ownIndex == 1)
 	{
-		fWheelPerimeterRaspi = 0.0303358 * 3.1415;
+		fWheelPerimeterRaspi = 0.0297898 * 3.1415;
 	}
 	else
 	{
-		fWheelPerimeterRaspi = 0.0303359 * 3.1415;
+		fWheelPerimeterRaspi = 0.0294919  * 3.1415;
 	}
 }
 
